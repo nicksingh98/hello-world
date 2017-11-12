92f5e8fd86715719486f4d8a5f10d297b444a30f
nicksingh98 <33602884+nicksingh98@users.noreply.github.com>
2017-11-12 17:52:31 +0000

    Create or_explainer.py

---
+++ or_explainer.py
@@ -0,0 +1,14 @@
+print("What is X?")
+X=str(input())
+print("X is "+X)
+print("What is Y?")
+Y=str(input())
+print("Y is " +Y)
+
+print("Hey! Is either of X or Y true? Y/N")
+Z=input()
+
+if Z == "Y":
+  bool_Q=True
+  else:
+    bool_Q=False
